f6e52e3f3a28172b14565a6c542a3d10ff9d7b7b
Elivelton <eliveltonpw@live.com>
2019-06-24 20:47:36 -0300

    update

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,22 @@
 public class Main {
 
     public static void main(String[] args) {
-        DLinkedList<String> DLinkedList = new DLinkedList<String>();
-        DLinkedList.addFirst("a");
-        DLinkedList.addFirst("b");
-        System.out.println("Tamanho: " + DLinkedList.size());
-        System.out.println("Está vazia? " + DLinkedList.isEmpty());
-        System.out.println(DLinkedList.getFirst());
-        System.out.println(DLinkedList.getLast());
-
+        DLinkedList<String> listD = new DLinkedList<String>();
+        listD.addFirst("e");
+        listD.addFirst("h");
+        listD.add(1, "bcd");
+        listD.addLast("a");
+        listD.addLast("b");
+        System.out.println(listD.getFirst());
+        listD.removeFirst();
+        System.out.println(listD.getFirst());
+        System.out.println(listD.getLast());
+        listD.removeLast();
+        System.out.println(listD.getLast());
+        System.out.println(listD.contains("e"));
+        System.out.println(listD.get(1));
+        System.out.println(listD.remove(0));
+        System.out.println(listD.size());
+        System.out.println(listD.isEmpty());
     }
 }
